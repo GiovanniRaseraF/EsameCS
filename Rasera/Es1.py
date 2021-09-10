f388aabc1e2ddba767aeef7cf3b9002c29604d69
Giovanni Rasera <45256169+GiovanniRaseraF@users.noreply.github.com>
2021-09-10 10:38:01 +0200

    Es1 Prima parte

--- Rasera/Es1.py
+++ Rasera/Es1.py
@@ -1,17 +1,18 @@
 # Esercizio 1
-
 def realmin(emim):
     return 2**(-emi-1)
-
 
 def realmax(t, emax):
     return (2**emax)*(1  - 2**(-t))
 
-def Fzero(t, emax, emin):
+def F(t, emax, emin):
     return 2 * (2 - 1) * 2**(t-1) * (emax + emin + 1)
 
-def F(t, emax, emin):
-    return Fzero(t, emax, emin) + 1
+def Fpos(t, emax, emin):
+    return (2 - 1) * 2**(t-1) * (emax + emin + 1)
+
+def Fzero(t, emax, emin):
+    return 1 + F(t, emax, emin) 
 
 def U(t):
     return 1/2 * 2**(1-t)
@@ -22,7 +23,7 @@ def numeridenormalizzati(t):
 
 # Main
 ## Raccogliemento Clausole
-print("Verifica(f -> stop)")
+print("Clausole(f -> stop)")
 clausole = []
 while(True):
     clausola = input(">")
@@ -33,13 +34,14 @@ while(True):
         clausole.append(clausola)
 
 ## Verifica Clausole
-for t in range(1,20):
-    for ema in range(1, 20):
-        for emi in range(1, 20):
+for t in range(1,30):
+    for ema in range(1, 30):
+        for emi in range(1, 30):
             rmi = realmin(emi)
             rma = realmax(t, ema)
+            f = F(t, ema, emi)
             fz = Fzero(t, ema, emi)
-            f = (t, ema, emi)
+            fp = Fpos(t, ema, emi)
             u = U(t)
             nd = numeridenormalizzati(t)
 
@@ -51,3 +53,5 @@ for t in range(1,20):
 
             if check == True:
                 print("t:{}\nemax:{}\nemin:{}".format(t,ema,emi))
+
+## Seconda Parte
